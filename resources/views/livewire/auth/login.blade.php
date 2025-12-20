2f15d34dfc7b402a1263a27e491abae255bc3b7e
Aslam Mahmud Siam <amsiam990@gmail.com>
2025-12-20 20:19:37 +0600

    feat: Add password visibility toggle to password input fields in login and registration forms.

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -149,10 +149,32 @@ new #[Layout('components.layouts.auth')] class extends Component {
                 @enderror
             </div>
 
-            <div>
+            <div x-data="{ show: false }">
                 <label for="password" class="block text-sm font-semibold text-gray-700 mb-2">Password</label>
-                <input wire:model="password" type="password" id="password" placeholder="Enter your password"
-                    class="w-full px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-custom-pink focus:ring-2 focus:ring-custom-pink/20 transition-all duration-300" />
+                <div class="relative" style="position: relative;">
+                    <input wire:model="password" :type="show ? 'text' : 'password'" id="password"
+                        placeholder="Enter your password"
+                        class="w-full px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-custom-pink focus:ring-2 focus:ring-custom-pink/20 transition-all duration-300 pr-10" />
+                    <button type="button" @click="show = !show"
+                        class="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500 hover:text-custom-pink focus:outline-none"
+                        style="position: absolute; top: 0; bottom: 0; right: 0; display: flex; align-items: center;">
+                        <svg x-show="!show" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
+                                clip-rule="evenodd" />
+                            <path
+                                d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
+                        </svg>
+                        <svg x-show="show" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                            fill="currentColor" style="display: none;">
+                            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                            <path fill-rule="evenodd"
+                                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
                 @error('password')
                     <span class="text-sm text-red-500 mt-1 block">{{ $message }}</span>
                 @enderror
@@ -176,8 +198,8 @@ new #[Layout('components.layouts.auth')] class extends Component {
                 <label class="block text-sm font-semibold text-gray-700">Verification Code</label>
                 <div class="flex items-center gap-4">
                     <img src="{{ route('captcha.image') }}?v={{ $captchaCode }}" alt="CAPTCHA Code"
-                        class="h-16 rounded-lg shadow-lg border-2 border-gray-200"
-                        wire:key="captcha-{{ $captchaCode }}" id="captcha-image">
+                        class="h-16 rounded-lg shadow-lg border-2 border-gray-200" wire:key="captcha-{{ $captchaCode }}"
+                        id="captcha-image">
                     <button type="button" wire:click="generateCaptcha"
                         class="px-4 py-2 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200 transition-colors duration-200 font-medium text-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-1" fill="none"
